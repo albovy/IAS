bb84b4135b7fa771b4c506d43a1533c1a2b3d9cc
Alejandro <abvieitez2@esei.uvigo.es>
2020-10-13 09:42:43 +0200

    JWT and register funcionality

---
+++ IASServer/models/User.js
@@ -0,0 +1,46 @@
+const mongoose = require("mongoose");
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: 'Password can\'t be empty',
+        minlength: [4, 'Password must be atleast 4 character long']
+    },
+    saltSecret: String
+});
+
+// Events
+userSchema.pre('save', function (next) {
+    bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(this.password, salt, (err, hash) => {
+            this.password = hash;
+            this.saltSecret = salt;
+            next();
+        });
+    });
+});
+
+
+// Methods
+userSchema.methods.verifyPassword = function (password) {
+    return bcrypt.compareSync(password, this.password);
+};
+
+userSchema.methods.generateJwt = function () {
+    return jwt.sign({
+            _id: this._id
+        },
+        process.env.JWT_SECRET, {
+            expiresIn: process.env.JWT_EXP
+        });
+}
+
+module.exports = mongoose.model("User", userSchema)
--- IASServer/routes/login.js
+++ IASServer/routes/login.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController')
 
-router.post('/', userController.register);
+router.post('/',userController.login);
 
 module.exports = router;
