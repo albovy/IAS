23898dc1ee5dceb36f66f976e3f922644b6e3e84
Alejandro <abvieitez2@esei.uvigo.es>
2020-10-13 09:42:43 +0200

    JWT and register funcionality

--- IASServer/app.js
+++ IASServer/app.js
@@ -1,34 +1,63 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const jwt = require('express-jwt')
+const unless = require('express-unless');
+const mongoose = require("mongoose");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const config = require('./config.json')
 
-var app = express();
+const indexRouter = require('./routes/index');
+const loginRouter = require('./routes/login')
+const usersRouter = require('./routes/users');
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+const app = express();
+
+
+const mongoDB = "mongodb://127.0.0.1:27017/iasdb";
+//BD
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true});
+mongoose.Promise = global.Promise;
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
+
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({
+  extended: false
+}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+process.env.JWT_SECRET = config.secret
+process.env.JWT_EXP = config.exp
+
+const jwtCheck = jwt({
+  secret: process.env.JWT_SECRET,
+  algorithms: ['HS256']
+});
+
+jwtCheck.unless = unless;
+app.use(jwtCheck.unless({
+  path: ['/api/login', {
+    url: '/api/users',
+    methods: ['POST']
+  }]
+}));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/api/login',loginRouter);
+app.use('/api/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
---
+++ IASServer/controllers/userController.js
@@ -0,0 +1,44 @@
+const User = require("../models/User");
+
+class UserController {
+  constructor() {}
+
+  register(req, res, next) {
+    let user = new User();
+    console.log(req.body);
+    user.username = req.body.username;
+    user.password = req.body.password;
+    console.log(req.body);
+    console.log("holaa");
+
+    if (JSON.stringify(req.body) == "{}") {
+      return res.status(404).send(["Body empty"]);
+    } else {
+      user.save((err, doc) => {
+        console.log("cachis");
+        if (!err) {
+          return res.status(200).send([doc]);
+        } else {
+          console.log(err);
+          if (err.code == 11000) {
+            return res.status(422).send(["Duplicate username found."]);
+          }
+          return next(err);
+        }
+      });
+    }
+  }
+
+  login(req, res, next) {
+    User.findOne({ username: req.body.username }, (err, user) => {
+      if (err) return next(err);
+      else if (!user) return res.status(404).send(["User not found"]);
+      else if (!user.verifyPassword(req.body.password))
+        return res.status(403).send(["Password doesn't match"]);
+      else if (user) return res.status(200).json({ token: user.generateJwt() });
+      else next();
+    });
+  }
+}
+
+module.exports = new UserController();
