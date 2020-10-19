f3e09e4a3a851e1fb30a149f51417d1fecf6f3e9
Alejandro <abvieitez2@esei.uvigo.es>
2020-10-19 20:17:03 +0200

    Fixed some bugs

--- IASServer/app.js
+++ IASServer/app.js
@@ -1,7 +1,7 @@
 const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
-const cookieParser = require('cookie-parser');
+
 const logger = require('morgan');
 const jwt = require('express-jwt')
 const unless = require('express-unless');
